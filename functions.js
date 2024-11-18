515bbd7d43a413162998b7c6e70bcb2f223e699d
Francisco Duarte <franciscoperes312@gmail.com>
2024-11-18 14:39:28 +0000

    Added "restaurantes" section

--- functions.js
+++ functions.js
@@ -9,15 +9,3 @@ function mudarImagemSlider(){
         });
     });
 }   
-
-function mudarImagemSliderAtracoes(){
-    const div_centro = document.getElementById("cartoes_atracoes");
-    const botoes = document.querySelectorAll(".slider_botoes button");
-
-    botoes.forEach(button => {
-        button.addEventListener("click", () => {
-            const imageUrl = button.getAttribute("data-image");
-            div_centro.style.backgroundImage = imageUrl;
-        });
-    });
-}
