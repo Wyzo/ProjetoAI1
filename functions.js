305cfbeb5dc1c92163c8cf0f74f07756999a75e7
Francisco Duarte <franciscoperes312@gmail.com>
2024-11-22 19:56:19 +0000

    Added a "clock" system to each page

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 function iniciarSliderAutomatico() {
-    const div_centro = document.getElementById("centro");
+    const div_centro = document.getElementById("centro_index");
     const botoes = document.querySelectorAll(".slider_botoes .slider_botao");
 
     const imagens = Array.from(botoes).map(button => button.getAttribute("data-image"));
@@ -19,3 +19,21 @@ function iniciarSliderAutomatico() {
 document.addEventListener("DOMContentLoaded", () => {
     iniciarSliderAutomatico();
 });
+
+function mostrarHoras(){
+    const horasAtuais = new Date();
+    const paragrafoHoras = document.getElementById("horas");
+
+    const horas = horasAtuais.getHours();
+    const minutos = horasAtuais.getMinutes();
+    const segundos = horasAtuais.getSeconds();
+
+    const horasFormatadas = String(horas).padStart(2, '0');
+    const minutosFormatados = String(minutos).padStart(2, '0');
+    const segundosFormatados = String(segundos).padStart(2, '0');
+
+    paragrafoHoras.textContent = `Horas: ${horasFormatadas}:${minutosFormatados}:${segundosFormatados}`;
+}
+
+setInterval(mostrarHoras, 1000);
+mostrarHoras();
